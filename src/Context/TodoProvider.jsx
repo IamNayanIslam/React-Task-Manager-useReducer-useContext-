695e4dd6b8ba3e17d4b6a318ec69d7c6c1d51519
IamNayanIslam <iamnayanislam@gmail.com>
2023-12-05 21:34:48 +0600

    Added local storage

--- src/Context/TodoProvider.jsx
+++ src/Context/TodoProvider.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { todoReducer } from "../Reducer/TodoReducer";
 
 export const TodoContext = createContext();
@@ -7,7 +7,14 @@ const INITIAL_TODO_STATE = {
   todos: [],
 };
 export const TodoProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(todoReducer, INITIAL_TODO_STATE);
+  const [state, dispatch] = useReducer(
+    todoReducer,
+    JSON.parse(localStorage.getItem("state")) || INITIAL_TODO_STATE
+  );
+
+  useEffect(() => {
+    localStorage.setItem("state", JSON.stringify(state));
+  }, [state]);
   const values = { state, dispatch };
   return <TodoContext.Provider value={values}>{children}</TodoContext.Provider>;
 };
